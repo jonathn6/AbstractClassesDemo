b102c36520a9ccb975ea6931f047e8fa18b381a4
jonathn6 <jonathn6@icloud.com>
2021-11-10 13:20:00 -0500

    complete todo list

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Car : Vehicle
+    {
+        public Car()
+        {
+
+        }
+        public bool HasTrunk { get; set; } = true;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("this car is in drive");
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -33,21 +33,65 @@ namespace ConsoleUI
 
             // Create a list of Vehicle called vehicles
 
+            var vehicles = new List<Vehicle>();
+
+
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
 
+            var focus = new Car() { Make="Ford", Model="Focus", Year=2021, HasTrunk=true };
+            var motorcycle = new Motorcycle() { Make="Kawasaki", Model="Ninja", Year=2020, HasSideCart=true};
+
+            var sedan = new Car() { HasTrunk=true, Make="Dodge", Model="Blah", Year=1999};
+            var sportCar = new Car() { HasTrunk=false, Make="Chevy", Model="Corvette", Year=1963};
+
+
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
 
+            vehicles.Add(focus);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(sportCar);
+
+            foreach (var vehicle in vehicles)
+            {
+                Console.Write($"There is a {vehicle.Year} {vehicle.Make} {vehicle.Model} in the list. ");
+                if (vehicle is Car)
+                {
+                    var tempCar = new Car();
+                    tempCar = (Car)vehicle;
+                    if (tempCar.HasTrunk == true)
+                    {
+                        Console.WriteLine("This car also has a trunk.");
+                    } else
+                    {
+                        Console.WriteLine("This car does not have a trunk.");
+                    }
+                } else
+                {
+                    var tempMotorcycle = new Motorcycle();
+                    tempMotorcycle = (Motorcycle)vehicle;
+                    if (tempMotorcycle.HasSideCart == true)
+                    {
+                        Console.WriteLine("This motorcycle also has a sidecar.");
+                    } else
+                    {
+                        Console.WriteLine("This motorcycle does not have a sidecar.");
+                    }
+                }
+                vehicle.DriveAbstract();
+                vehicle.DriveVirtual();
+            }
+
             // Call each of the drive methods for one car and one motorcycle
 
             #endregion            
-            Console.ReadLine();
         }
     }
 }
